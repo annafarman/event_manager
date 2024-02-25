721a094b84a0515e850e34b850c084efc5e20b21
Anna Farman <anna.farman@gmail.com>
2024-02-25 13:59:41 -0500

    Access columns by the name using :symbols

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,14 @@
 require 'csv' #for RUBY CSV
 puts 'Event Manager Initialized!'
 
-contents = CSV.open('event_attendees.csv', headers: true)
+contents = CSV.open(
+    'event_attendees.csv', 
+    headers: true,
+    header_converters: :symbol
+    )
+
 contents.each do |row|
-    name = row[2]
-    puts name
+    name = row[:first_name]
+    zipcode = row[:zipcode]
+    puts "#{name} #{zipcode}"
 end

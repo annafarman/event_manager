131ce6395c43df49d5c84741d3cb8342132612de
Anna Farman <anna.farman@gmail.com>
2024-02-25 13:55:27 -0500

    Switch to Ruby CSV library

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,29 +1,8 @@
+require 'csv' #for RUBY CSV
 puts 'Event Manager Initialized!'
 
-contents = File.read('event_attendees.csv')
-
-# puts contents
-# puts File.exist? 'event_attendees.csv'
-
-# Ruby’s String#split allows you to convert a string of text into an array along a particular character. By default when you send the split message to the String without a parameter it will break the string apart along each space " " character. Therefore, we need to specify the comma "," character to separate the columns.
-
-# lines = File.readlines('event_attendees.csv')
-# lines.each do |line|
-#     # puts line
-#     columns = line.split(",")
-#     # p columns
-#     name = columns[2]
-#     puts name
-# end
-
-#exclude the first row
-
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
-    # puts line
-    next if index == 0
-    columns = line.split(",")
-    # p columns
-    name = columns[2]
+contents = CSV.open('event_attendees.csv', headers: true)
+contents.each do |row|
+    name = row[2]
     puts name
 end

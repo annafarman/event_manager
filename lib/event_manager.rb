c5beb3df64c5c3040abe846be62227dd3e346974
Anna Farman <anna.farman@gmail.com>
2024-02-27 18:24:03 -0500

    Clean zipcode function

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,22 @@
 require 'csv' #for RUBY CSV
+
 puts 'Event Manager Initialized!'
+
+
+
+def clean_zipcode(zipcode)
+    # if zipcode.nil?
+    #     zipcode = '00000'
+    # elsif zipcode.length < 5
+    #     zipcode = zipcode.rjust(5, '0')
+    # elsif zipcode.length > 5
+    #     zipcode = zipcode[0..4]
+    # else
+    #     zipcode
+    # end
+
+    zipcode.to_s.rjust(5, '0')[0..4]
+end
 
 contents = CSV.open(
     'event_attendees.csv', 
@@ -10,5 +27,8 @@ contents = CSV.open(
 contents.each do |row|
     name = row[:first_name]
     zipcode = row[:zipcode]
+
+    zipcode = clean_zipcode(zipcode)
+
     puts "#{name} #{zipcode}"
 end
